f4f1790e118363374957a27a8193efdcaa160de0
matheuscarv69 <matheus9126@gmail.com>
2021-01-21 15:07:19 -0400

    :sparkles: feat: Test Suite created

---
+++ src/main/java/SuiteTests.java
@@ -0,0 +1,19 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestGoogle.class,
+        TesteWindows.class,
+        TesteFrames.class,
+        TesteAlert.class,
+        TesteCampoTreinamento.class,
+        TesteDesafioForm.class,
+        TesteBusinessRules.class,
+        TesteRulesRegisterDDT.class
+
+})
+public class SuiteTests {
+
+
+}
