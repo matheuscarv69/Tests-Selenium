d6dd370dee42c3c8e792e43c2ab7628d8b8c7ca5
matheuscarv69 <matheus9126@gmail.com>
2021-01-22 18:11:37 -0400

    :sparkles: feat: Take screenshot at the end of the test

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,14 +1,30 @@
 package core;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
+
+import static core.DriverFactory.getDriver;
 import static core.DriverFactory.killDriver;
 
 public class BaseTest {
 
+    @Rule
+    public TestName testName = new TestName();
 
     @After
-    public void finalizeWebDriver() {
+    public void finalizeWebDriver() throws IOException {
+        TakesScreenshot ss = (TakesScreenshot) getDriver();
+        File arquivo = ss.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(arquivo, new File("target" + File.separator + "screenshot" +
+                File.separator + testName.getMethodName() + ".jpg"));
+
         /**
          * Verificação da propriedade -> Properties.class
          *
